a008ce7fa2137f026e76877c158682f019a4e62a
tinnguyen <108519316+tinboy16@users.noreply.github.com>
2024-10-25 02:38:47 -0700

    Update autotitan.sh

--- autotitan.sh
+++ autotitan.sh
@@ -16,11 +16,19 @@ mkdir -p ~/.titanedge
 echo "Starting Docker container with volume and network settings..."
 docker run --network=host -d -v ~/.titanedge:/root/.titanedge nezha123/titan-edge
 
-# Wait for 8 seconds before running the bind command
-echo "Waiting for 8 seconds..."
+# Wait for 10 seconds before running the bind command
+echo "Waiting for 10 seconds..."
 sleep 10
 
-# Run the binding command
+# Check if the container is running
+container_id=$(docker ps -q --filter ancestor=nezha123/titan-edge)
+
+if [ -z "$container_id" ]; then
+    echo "Error: Titan Edge container is not running."
+    exit 1
+fi
+
+# Run the binding command separately
 echo "Binding device with hash..."
 docker run --rm -it -v ~/.titanedge:/root/.titanedge nezha123/titan-edge bind --hash=D7E0A5A7-D1FF-4451-9DEF-7D76E911D31E https://api-test1.container1.titannet.io/api/v2/device/binding
 
